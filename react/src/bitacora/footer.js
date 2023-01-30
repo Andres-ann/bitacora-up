4819151047eb20e875c9688ab7199c18a8d89e0b
Andres <andresdngtt@gmail.com>
2023-01-30 08:40:56 -0300

    Se agrega version Node a package.json

--- react/src/bitacora/footer.js
+++ react/src/bitacora/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 		<div className='container'>
 			<footer className='py-3 my-4'>
 				<p className='text-center text-muted text-small'>
-					Todos los derechos reservados &copy; Bitácora UP by Nachito Robinson || Desarrollado por
+					Bitácora UP by Nachito Robinson &copy; Todos los derechos reservados || Desarrollado por
 					<span>
 						<a href='https://github.com/Andres-ann' className='text-muted ms-1' target='_blank'>
 							@andres.ndev
