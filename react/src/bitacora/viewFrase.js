258fe89357ee9eb973582bc72f62118eae052256
Andrés Negrette <80859408+Andres-ann@users.noreply.github.com>
2023-03-03 11:57:51 -0300

    Update hashtags

--- react/src/bitacora/viewFrase.js
+++ react/src/bitacora/viewFrase.js
@@ -111,7 +111,7 @@ const ViewFraseComponent = () => {
 
 								<div className='text-primary mt-5 pt-2'>
 									<small>
-										#LaLoooooz #TresTirosEnLaRodilla #VolvieronLosMemes #UnionPersonal #Upcn #yLasParitarias? #VivaPeron 
+										#OportunidadesDeMejora #LaLoooooz #TresTirosEnLaRodilla #VolvieronLosMemes #UnionPersonal #Upcn #yLasParitarias? #VivaPeron 
 										#QueVuelvaElHome #JuiraRodrigo #Allá #PiqueterosDuros #PeronismoMilitante
 									</small>
 								</div>
