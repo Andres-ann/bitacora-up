a7e6b6bd31f1d40575f4806d10da54dfc1ff4110
Andres <andresdngtt@gmail.com>
2023-03-08 15:18:46 -0300

    bitacora UP V 2.1.0

---
+++ react/build/precache-manifest.8018aaf4b7961d9a4937c6a8bd5eb055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "858b6c6b409e42c32d52",
+    "url": "/static/css/main.dc89ee87.chunk.css"
+  },
+  {
+    "revision": "858b6c6b409e42c32d52",
+    "url": "/static/js/main.858b6c6b.chunk.js"
+  },
+  {
+    "revision": "f9a280cce4a84bd8f31a",
+    "url": "/static/js/1.f9a280cc.chunk.js"
+  },
+  {
+    "revision": "4bc2892e0181c2b3d240",
+    "url": "/static/css/2.1f0d26b4.chunk.css"
+  },
+  {
+    "revision": "4bc2892e0181c2b3d240",
+    "url": "/static/js/2.4bc2892e.chunk.js"
+  },
+  {
+    "revision": "148dedf133217d189ab6",
+    "url": "/static/js/runtime~main.148dedf1.js"
+  },
+  {
+    "revision": "f1108c689622de129995ebe178b837ac",
+    "url": "/index.html"
+  }
+];
--- react/build/service-worker.js
+++ react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c740cd475ce4760e15a11a2e0607b0a8.js"
+  "/precache-manifest.8018aaf4b7961d9a4937c6a8bd5eb055.js"
 );
 
 workbox.clientsClaim();
