087e0863f467ef333cd66c417481b554914a50f7
Andres <andresdngtt@gmail.com>
2023-04-18 19:46:07 -0300

    Se crea la pagina de inicio, header y base del componente para ver la lista de items

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostItemComponent } from './components/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PostsComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
