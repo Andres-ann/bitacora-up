32ae1d15cdd84f98e6c1b73c7588d39d36130437
Andres Negrette <andresdngtt@gmail.com>
2023-04-20 23:49:43 -0300

    Se modifica el estilo de inicio y componente pagina de de vista individual

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './pages/create/create.component';
 import { EditComponent } from './pages/edit/edit.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ShowComponent } from './pages/show/show.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create', component: CreateComponent },
   { path: 'update/:id', component: EditComponent },
+  { path: 'view/:id', component: ShowComponent },
   { path: '**', redirectTo: '' },
 ];
 
