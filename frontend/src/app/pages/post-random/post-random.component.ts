18d0e1e87c5e216d8a1607d958730964f40a81f3
Andres <andresdngtt@gmail.com>
2023-05-17 13:53:21 -0300

    Se crea la funcion para ver una frase random, junto con su controlador en el  backend

--- frontend/src/app/pages/post-random/post-random.component.ts
+++ frontend/src/app/pages/post-random/post-random.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CrudService } from 'src/app/services/crud.service';
+import { RandomService } from 'src/app/services/random.service';
+import { ActivatedRoute } from '@angular/router';
+import { Frases } from 'src/app/models/frases.model';
 
 @Component({
   selector: 'app-post-random',
   templateUrl: './post-random.component.html',
-  styleUrls: ['./post-random.component.css']
+  styleUrls: ['./post-random.component.css'],
 })
 export class PostRandomComponent {
+  id!: any;
+  posts: Frases[] = [];
 
+  @Input() visualizaciones!: number;
+  @Output() postUpdated = new EventEmitter<Frases>();
+
+  constructor(
+    private crudService: CrudService,
+    private randomService: RandomService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.randomService.getFraseRandom().subscribe((res) => {
+      this.posts = res;
+    });
+  }
 }
