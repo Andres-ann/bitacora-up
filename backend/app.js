a7d0cbc0b3634591691cac4f79bd581d9cac959b
Andres <andresdngtt@gmail.com>
2023-05-17 13:54:30 -0300

    Se crea el controlador que devuelve ina frase random

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import frasesRouter from './routes/frasesRoutes.js';
+import randomRouter from './routes/randomRoutes.js';
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use('/api/frases', frasesRouter);
+app.use('/api/random', randomRouter);
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
