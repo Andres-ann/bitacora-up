161b13b9154437f8e871bae7fce30b8e73199fb3
Andres Negrette <andresdngtt@gmail.com>
2023-05-17 21:20:59 -0300

    Se corrige el componente de busqueda para que traiga resultados parciales al escribir en el input

--- frontend/src/app/pages/search/search.component.ts
+++ frontend/src/app/pages/search/search.component.ts
@@ -22,20 +22,53 @@ export class SearchComponent implements OnInit {
   search() {
     clearTimeout(this.timer);
 
-    // Mostrar el mensaje de "No se encontraron resultados" después de 3 segundos
+    // Mostrar el mensaje de "No se encontraron resultados"
     this.timer = setTimeout(() => {
       this.showNoResultsMessage = true;
     }, 500);
 
     this.searchService.searchCollection(this.searchText).subscribe(
       (results: Frases[]) => {
-        this.searchResults = results;
-        this.showNoResultsMessage = false; // Ocultar el mensaje si se encontraron resultados
+        // Filtrar los resultados de acuerdo a los diferentes tipos de coincidencias
+        this.searchResults = this.filterSearchResults(results);
+        this.showNoResultsMessage = this.searchResults.length === 0;
       },
       (error: any) => {
         this.showNoResultsMessage = false; // Ocultar el mensaje en caso de error
         console.error(error);
       }
     );
+  }
+
+  filterSearchResults(results: Frases[]): Frases[] {
+    const filteredResults: Frases[] = [];
+
+    if (this.searchText.length === 0) {
+      return results; // Si no hay texto de búsqueda, devolver todos los resultados
+    }
+
+    const regex = new RegExp(this.escapeRegExp(this.searchText), 'i'); // Expresión regular para la búsqueda (ignora mayúsculas y minúsculas)
+
+    for (const result of results) {
+      const lowercaseTitle = result.frase.toLowerCase();
+      const lowercaseContent = result.autor.toLowerCase();
+
+      // Verificar coincidencia en el título o el contenido utilizando la expresión regular
+      if (regex.test(lowercaseTitle) || regex.test(lowercaseContent)) {
+        filteredResults.push(result);
+      }
+    }
+
+    return filteredResults;
+  }
+
+  escapeRegExp(text: string) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
+
+  clearSearch() {
+    this.searchText = '';
+    this.searchResults = [];
+    this.showNoResultsMessage = false;
   }
 }
