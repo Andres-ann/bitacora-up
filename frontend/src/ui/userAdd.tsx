6dee1a4f1c249a326fdc6c8e6b5ee48f0ab33043
Andres-ann <andres.ndev@gmail.com>
2024-12-12 23:07:31 -0300

    feat: crear componente para agregar frases desde home

---
+++ frontend/src/ui/userAdd.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Avatar } from '@nextui-org/react';
+import Link from 'next/link';
+
+export default function UserAdd() {
+  return (
+    <Link href="/add">
+      <div className="flex items-center space-x-4 p-4">
+        {/* Avatar */}
+        <Avatar size="lg" name="App" />
+
+        {/* Información del usuario */}
+        <div>
+          <p className="font-semibold text-gray-900">Nombre Apellido</p>
+          <p className="text-sm text-gray-500">¿Qué novedades hay?</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
