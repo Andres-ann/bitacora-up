dac4b58e7a6073bf200f58ef8466f11d449a365f
Andres-ann <andres.ndev@gmail.com>
2024-12-21 12:50:01 -0300

    feat: separar la logica del componente postCard para que sea reutilizable

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,19 @@
 import Avatar from '@/ui/avatar';
 import DarkModeBtn from '@/ui/darkmodeBtn';
 import LoadMorePostBtn from '@/ui/loadMorePostBtn';
-import PostCard from '@/ui/postCard';
+import FrasesList from '@/ui/frasesList';
 import UserAdd from '@/ui/userAdd';
 import { Divider } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen overflow-hidden  flex-1 overflow-y-auto scrollbar-hide">
+    <div className="flex flex-col w-full h-screen overflow-hidden flex-1 overflow-y-auto scrollbar-hide">
       <DarkModeBtn />
       <Avatar />
       <UserAdd />
       <Divider />
       <div className="pb-16">
-        <PostCard />
+        <FrasesList />
         <LoadMorePostBtn />
       </div>
     </div>
--- frontend/src/ui/postCard.tsx
+++ frontend/src/ui/postCard.tsx
@@ -3,7 +3,6 @@
 import { Icon } from '@iconify-icon/react';
 import { Avatar, Divider } from '@nextui-org/react';
 import PostActions from '@/ui/postActions';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 interface Usuario {
@@ -22,101 +21,57 @@ interface Frase {
   usuarioId?: Usuario;
 }
 
-export default function PostCard() {
-  const [frases, setFrases] = useState<Frase[]>([]);
+interface PostCardProps {
+  frase: Frase;
+  onLike: (id: string) => void;
+}
 
-  useEffect(() => {
-    const fetchFrases = async () => {
-      try {
-        const response = await fetch('/api/frases');
-        const data = await response.json();
-        setFrases(data.docs);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchFrases();
-  }, []);
-
-  const handleLike = async (id: string) => {
-    try {
-      const res = await fetch('/api/addlike', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id }),
-      });
-
-      if (!res.ok) {
-        throw new Error('Failed to like the frase');
-      }
-
-      setFrases((prevFrases) =>
-        prevFrases.map((frase) =>
-          frase._id === id ? { ...frase, likes: frase.likes + 1 } : frase
-        )
-      );
-    } catch (error) {
-      console.error('Error liking frase:', error);
-    }
+export default function PostCard({ frase, onLike }: PostCardProps) {
+  const usuario = {
+    name: frase.usuarioId?.name || 'Bitacora UP',
+    username: frase.usuarioId?.username || 'bitacoraup',
+    avatar: frase.usuarioId?.avatar || 'https://i.ibb.co/ZNyjQ2g/favicon.jpg',
   };
 
   return (
-    <>
-      {frases.map((frase) => {
-        const usuario = {
-          name: frase.usuarioId?.name || 'Bitacora UP',
-          username: frase.usuarioId?.username || 'bitacoraup',
-          avatar:
-            frase.usuarioId?.avatar || 'https://i.ibb.co/ZNyjQ2g/favicon.jpg',
-        };
+    <div className="p-4">
+      <div className="flex items-start space-x-4">
+        {/* Avatar del usuario */}
+        <Avatar
+          size="md"
+          className="shadow-lg"
+          name={usuario.avatar}
+          src={usuario.avatar}
+        />
 
-        return (
-          <div key={frase._id} className="p-4">
-            <div className="flex items-start space-x-4">
-              {/* Avatar */}
-              <Avatar
-                size="md"
-                className="shadow-lg"
-                name={usuario.avatar}
-                src={usuario.avatar}
-              />
-
-              {/* Información del usuario y post */}
-              <div className="flex-1">
-                <div className="flex items-center">
-                  <p className="text-sm font-medium text-gray-900">
-                    {usuario.name}
-                  </p>
-                  <Icon
-                    icon="bitcoin-icons:verify-filled"
-                    width="16"
-                    className="text-blue-400 align-middle"
-                  />
-                </div>
-                <p className="text-xs text-gray-500">@{usuario.username}</p>
-                <div className="p-4 mt-4 text-sm font-light rounded-md border">
-                  <Link href="/post">
-                    <p className="text-gray-900">{frase.frase}</p>
-                    <p className=" mt-6 text-gray-500">- {frase.autor}</p>
-                  </Link>
-                </div>
-                {/* Botones de like, comentar y compartir */}
-                <PostActions
-                  likes={frase.likes}
-                  comments={frase.comentarios.length}
-                  views={frase.visualizaciones}
-                  id={frase._id}
-                  onLike={handleLike}
-                />
-              </div>
-            </div>
-            <Divider />
+        {/* Contenido del post */}
+        <div className="flex-1">
+          <div className="flex items-center">
+            <p className="text-sm font-medium text-gray-900">{usuario.name}</p>
+            <Icon
+              icon="bitcoin-icons:verify-filled"
+              width="16"
+              className="text-blue-400 align-middle"
+            />
           </div>
-        );
-      })}
-    </>
+          <p className="text-xs text-gray-500">@{usuario.username}</p>
+          <div className="p-4 mt-4 text-sm font-light rounded-md border">
+            <Link href="/post">
+              <p className="text-gray-900">{frase.frase}</p>
+              <p className="mt-6 text-gray-500">- {frase.autor}</p>
+            </Link>
+          </div>
+          {/* Botones de interacción */}
+          <PostActions
+            likes={frase.likes}
+            comments={frase.comentarios.length}
+            views={frase.visualizaciones}
+            id={frase._id}
+            onLike={onLike}
+          />
+        </div>
+      </div>
+      <Divider />
+    </div>
   );
 }
