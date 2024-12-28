642c5c63acd82dc2a0aec688d473778524adaa68
Andres-ann <andres.ndev@gmail.com>
2024-12-27 21:19:41 -0300

    feat: incorporar header y navbar

--- frontend/src/app/(frases)/search/page.tsx
+++ frontend/src/app/(frases)/search/page.tsx
@@ -1,3 +1,15 @@
+import Header from '@/ui/header';
+import Navbar from '@/ui/navbar';
+import { Divider } from '@nextui-org/react';
+
 export default function Search() {
-  return <h1>Search page</h1>;
+  return (
+    <>
+      <div className="flex flex-col w-full h-screen overflow-hidden flex-1 overflow-y-auto scrollbar-hide">
+        <Header title="Buscar" />
+        <Divider />
+        <Navbar />
+      </div>
+    </>
+  );
 }
