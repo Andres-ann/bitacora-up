dd7109b8461a6e362e8f6e469f9cc76952243680
Andres-ann <andres.ndev@gmail.com>
2024-12-27 23:00:28 -0300

    feat: incorporar componente y funcionalidad en pagina de busqueda

---
+++ frontend/src/ui/search.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { Input } from '@nextui-org/react';
+import { Icon } from '@iconify-icon/react';
+
+export default function Search({
+  onSearch,
+}: {
+  onSearch: (query: string) => void;
+}) {
+  const [query, setQuery] = useState('');
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    const newTimeoutId = setTimeout(() => {
+      onSearch(e.target.value);
+    }, 500); // 500ms de retraso para mejorar UX
+    setTimeoutId(newTimeoutId);
+  };
+
+  return (
+    <div className="p-4 flex items-center w-full">
+      <Input
+        value={query}
+        onChange={handleChange}
+        classNames={{
+          base: 'max-w-full h-10',
+          mainWrapper: 'h-full',
+          input: 'text-small',
+          inputWrapper:
+            'h-full font-normal bg-default-100 hover:bg-default-200',
+        }}
+        placeholder="Buscar..."
+        size="sm"
+        startContent={
+          <div className="text-gray-500 pointer-events-none flex items-center p-2">
+            <Icon icon="iconamoon:search-bold" width="16" />
+          </div>
+        }
+        type="text"
+      />
+    </div>
+  );
+}
