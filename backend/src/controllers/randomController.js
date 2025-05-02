976bb9c00da68625c695dfebf0fec52d2538a2bf
Andres-ann <andres.ndev@gmail.com>
2025-05-02 20:47:02 -0300

    fix: popular datos de correctamente en el endpoint

--- backend/src/controllers/randomController.js
+++ backend/src/controllers/randomController.js
@@ -2,13 +2,32 @@ import { frasesModel } from '../models/frasesModel.js';
 
 export const getFraseRandom = async (req, res) => {
   try {
-    const fraserandom = await frasesModel.aggregate([{ $sample: { size: 1 } }]);
+    const frasesRandom = await frasesModel.aggregate([
+      { $sample: { size: 1 } },
+    ]);
 
-    if (fraserandom.length === 0) {
+    if (frasesRandom.length === 0) {
       return res.status(404).json({ message: 'No phrases found' });
     }
 
-    res.status(200).json(fraserandom[0]);
+    const fraseId = frasesRandom[0]._id;
+
+    const frase = await frasesModel
+      .findById(fraseId)
+      .populate('usuarioId', 'name username avatar')
+      .populate({
+        path: 'comentarios',
+        populate: {
+          path: 'usuarioId',
+          select: 'name username avatar',
+        },
+      });
+
+    if (!frase) {
+      return res.status(404).json({ message: 'Phrase not found' });
+    }
+
+    res.status(200).json(frase);
   } catch (error) {
     console.error(error);
     res.status(500).json({
